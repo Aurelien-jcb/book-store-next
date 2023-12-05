03c15f48688ff2a6040347ff57791828fe8e6495
Aurelien-jcb <jacob-aurelien@hotmail.fr>
2023-12-05 17:54:55 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Book store
+The Book Store project is a frontend interface developed to manage user's books.
+
+## Stack
+Book Store was developed using the following technologies:
+
+  - Next.js
+  - Tailwind CSS
+  - Typescript
+  - JWT
+
---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Book store',
+  description: 'A book store application made with Next.js, typescript and Tailwind css',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="fr">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
